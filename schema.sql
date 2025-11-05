248fea911ef147a0dc68b0234cc92b960d729b58
shoyaah <johnmarkdimacs@gmail.com>
2025-11-05 12:11:43 +0800

    GROUP 2 INSERTED TEACHER'S VALUE

--- schema.sql
+++ schema.sql
@@ -1,2 +1,5 @@
 CREATE DATABASE school_db;USE school_db;CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY,name VARCHA(100),grade INT);
 CREATE TABLE teachers(id INT, name VARCHAR(50), subject VARCHAR(50));
+INSERT INTO teachers (id, name, subject)
+VALUES(1, 'Marielle Coronel','DBMS'),
+(2, 'Raymond Balba', 'Calculus based Physics');
