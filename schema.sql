16ea1a42e4cecdaf987271ffccd6385ea0bbd4a4
Ryan1017-p <24-33376@g.batstate-u.edu.ph>
2025-11-05 11:59:42 +0800

    Group 1 added teachers table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,2 @@
 CREATE DATABASE school_db;USE school_db;CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY,name VARCHA(100),grade INT);
+CREATE TABLE teachers(id INT, name VARCHAR(50), subject VARCHAR(50));
